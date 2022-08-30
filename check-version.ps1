09254a8c65a2776c765bacf2df1390070b9dda4a
thosoo <thieson08@me.com>
2022-08-30 11:56:22 +0200

    Update check-version.ps1

--- check-version.ps1
+++ check-version.ps1
@@ -1,7 +1,7 @@
 $repoName = "prusa3d/prusaslicer"
 $releasesUri = "https://api.github.com/repos/$repoName/releases/latest"
 $tag = (Invoke-WebRequest $releasesUri | ConvertFrom-Json).tag_name
-$tag2 = $tag.replace('version_','')
+$tag2 = $tag
 Write-Host $tag2
 if ($tag2 -match "alpha")
 {
