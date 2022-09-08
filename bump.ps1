3f286c7ccd903df4f7fe63b04e99a67e469864ac
thosoo <mail@thaison-nguyen.de>
2022-09-08 07:53:52 +0200

    Remove 32bit support

--- bump.ps1
+++ bump.ps1
@@ -33,28 +33,21 @@ else{
 
         $installer = Get-IniContent ".\PrusaSlicerPortable\App\AppInfo\installer.ini"
 
-        $asset1Pattern = "*+win32*"
+        $asset1Pattern = "*+win64*"
         $asset1 = (Invoke-WebRequest $releasesUri | ConvertFrom-Json).assets | Where-Object name -like $asset1Pattern
         $asset1Download = $asset1.browser_download_url.replace('%2B','+')
         $installer["DownloadFiles"]["DownloadURL"]=$asset1Download
         $installer["DownloadFiles"]["DownloadName"]=$asset1.name.replace('.zip','')
         $installer["DownloadFiles"]["DownloadFilename"]=$asset1.name
 
-        $asset2Pattern = "*+win64*"
-        $asset2 = (Invoke-WebRequest $releasesUri | ConvertFrom-Json).assets | Where-Object name -like $asset2Pattern
-        $asset2Download = $asset2.browser_download_url.replace('%2B','+')
-        $installer["DownloadFiles"]["Download2URL"]=$asset2Download
-        $installer["DownloadFiles"]["Download2Name"]=$asset2.name.replace('.zip','')
-        $installer["DownloadFiles"]["Download2Filename"]=$asset2.name
         $installer | Out-IniFile -Force -Encoding ASCII -Pretty -FilePath ".\PrusaSlicerPortable\App\AppInfo\installer.ini"
 
-        $appinfo["Control"]["BaseAppID"]=-join("%BASELAUNCHERPATH%\App\",$asset1.name.replace('.zip',''),"\prusa-slicer.exe")
-        $appinfo["Control"]["BaseAppID64"]=-join("%BASELAUNCHERPATH%\App\",$asset2.name.replace('.zip',''),"\prusa-slicer.exe")
+        $appinfo["Control"]["BaseAppID64"]=-join("%BASELAUNCHERPATH%\App\",$asset1.name.replace('.zip',''),"\prusa-slicer.exe")
+
         $appinfo | Out-IniFile -Force -Encoding ASCII -FilePath ".\PrusaSlicerPortable\App\AppInfo\appinfo.ini"
 
         $launcher = Get-IniContent ".\PrusaSlicerPortable\App\AppInfo\Launcher\PrusaSlicerPortable.ini"
-        $launcher["Launch"]["ProgramExecutable"]=-join($asset1.name.replace('.zip',''),"\prusa-slicer.exe")
-        $launcher["Launch"]["ProgramExecutable64"]=-join($asset2.name.replace('.zip',''),"\prusa-slicer.exe")
+        $launcher["Launch"]["ProgramExecutable64"]=-join($asset1.name.replace('.zip',''),"\prusa-slicer.exe")
         $launcher | Out-IniFile -Force -Encoding ASCII -FilePath ".\PrusaSlicerPortable\App\AppInfo\Launcher\PrusaSlicerPortable.ini"
         Write-Host "Bumped to "+$tag2
         echo "SHOULD_COMMIT=yes" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
